87e59daa020de397fd56dd36094a37e995947b59
yuto_h <y.hashido.1278@gmail.com>
2023-04-08 18:15:59 +0900

    パラメータの初期値を構成するファイル

---
+++ my_config.py
@@ -0,0 +1,7 @@
+class Config(object):
+    def __init__(self, test_size, seed, lgb_params):
+        self.test_size = test_size
+        self.seed = seed
+        self.lgb_params = lgb_params
+        
+
